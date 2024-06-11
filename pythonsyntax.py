71c745c0d47f6418c3327b188f57a44773c3c48d
Vanady-Beard <vanadybeard9@gmail.com>
2024-06-10 21:52:50 -0400

    first commit

---
+++ pythonsyntax.py
@@ -0,0 +1,62 @@
+# 1. Showcase Your Dance Moves!
+
+
+#Task1: Code Correction
+#weather = "sunny"
+
+#if weather == "sunny":
+#print("Wear sunglasses!")
+#else:
+#print("Take an umbrella!")
+
+
+weather = "sunny"
+
+if weather == "sunny":
+
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2: Your Mood Today
+
+feeling = input()
+if feeling == "happy":
+    print ("That's great to hear!")
+else: 
+    print ("I hope your day gets better!")
+
+
+#2. Python Data Types and type() Function
+
+#Task 1: Code Correction
+#variable_a = "Hello, World!"
+#variable_b = 23
+#variable_c = 3.14
+#variable_d = True
+
+
+variable_a = "Hello, World!"
+print (type(variable_a))
+
+variable_b = 23
+print (type(variable_b))
+
+variable_c = 3.14
+print (type(variable_c))
+
+variable_d = True
+print (type(variable_d))
+
+
+#Task 1: Grocery Store Math 
+#bread = 2.75
+#peanut butter= 4.50
+#jelly = 3.23
+
+
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+groceryprice = bread + peanut_butter + jelly
+print (groceryprice)
